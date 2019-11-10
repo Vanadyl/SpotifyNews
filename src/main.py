176f7e3ce09f6048d78846554ea784748c413344
XQING <vikas@igres.net>
2019-11-10 16:06:27 +0000

    Update Sprint 1

---
+++ src/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 03/11/2019 21:13
+# @Author  : XQING
+# @File    : main.py
+# @Software: PyCharm
---
+++ src/news.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 03/11/2019 21:21
+# @Author  : XQING
+# @File    : news.py
+# @Software: PyCharm
