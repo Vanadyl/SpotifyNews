d96012c50e55ed0e98ac5a0af019eaebfb44f0c5
Steeeeve98 <36635946+Steeeeve98@users.noreply.github.com>
2019-11-20 10:05:39 +0000

    Remove old code

--- src/spotifyTest.py
+++ src/spotifyTest.py
@@ -3,23 +3,6 @@ import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 import json
 
-#needs to be authorized with a token before this will work
-'''
-scope =''
-
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    #print "Usage: %s username" % (sys.argv[0],)
-    sys.exit()
-
-token = util.prompt_for_user_token(username,scope,client_id='90d2a3cd7ee04a97ac686fd3ffc',client_secret='dbcbbdfcc8004d218e0cdc42e80cbdf3',redirect_uri='http://localhost/')
-
-
-
-client_credentials_manager = SpotifyClientCredentials(client_id='90d2a3cd7ee04a97ac686fd3ffc', client_secret='dbcbbdfcc8004d218e0cdc42e80cbdf3')
-'''
-
 credentials = SpotifyClientCredentials(
     client_id='90d2a3cd7ee04a97ac686fd3ffcf13f6',
     client_secret='dbcbbdfcc8004d218e0cdc42e80cbdf3')
